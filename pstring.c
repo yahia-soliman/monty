e627efca4c86aa9fe6225bbbd34303adbe727949
Ehabw57 <maestro.ehab123@gmail.com>
2023-10-20 01:00:02 +0300

    Uploded pstring functions
    pchar: Prints int in ascii
    pstr: prints a whole string

---
+++ pstring.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+/**
+ * pstr_op - prints the string starting at the top of the stack
+ * followed by a new line.
+ * @list: Head of the list
+ * @line_num: line number
+ */
+void pstr_op(stack_t **list, unsigned int line_num)
+{
+	stack_t *move = *list;
+
+	for (; move != NULL && move->n != 0 && line_num; move = move->next)
+		putchar(move->n);
+	putchar('\n');
+}
+
+/**
+ * pchar_op - prints the character at the TOS
+ * @list: Head of the stack/queue
+ * @line_num: Line number of the instruction
+ */
+void pchar_op(stack_t **list, unsigned int line_num)
+{
+	int c;
+
+	if (*list == NULL)
+		syntax_error(12, list, line_num);
+	c = (*list)->n;
+	if (c > 127 || c < 0)
+		syntax_error(11, list, line_num);
+
+	putchar(c);
+	putchar('\n');
+}
